cbf598014de49c087cb84fa202494b981ec52418
Divyanshu Motivaras <divyanshu.motivaras@scaletech.xyz>
2024-08-01 09:37:55 +0530

    env

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,18 +2,23 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Blog } from './entities/blog.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      database: 'web-security',
-      password: 'password',
-      username: 'postgres',
-      port: 5432,
-      entities: [User, Blog],
-      synchronize: true,
-      logging: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [User, Blog],
+        logging: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
 })
